629d3a8560840c73ba42fc9a5cca2cbfd16efd48
Bektas SARI <94165627+bektas-sari@users.noreply.github.com>
2025-01-02 19:54:48 +0300

    Add files via upload

---
+++ python_basics.py
@@ -0,0 +1,15 @@
+print("Merhaba DÜnya")
+
+a = 10  # int
+b = 3.14  # float
+c = "Python"  # string
+d = [1, 2, 3]  # list
+print(type(a), type(b), type(c), type(d))
+
+
+for i in range(5):
+    print(f"Döngüdeyim: {i}")
+
+
+isim = input("Adınızı girin: ")
+print(f"Merhaba, {isim}!")
